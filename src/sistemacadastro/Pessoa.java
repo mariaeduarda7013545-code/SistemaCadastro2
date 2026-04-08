b289d6fc26106ee248842e9d1ccfb84924c20345
J0NH17 <1091771@senacminas.edu.br>
2026-04-08 20:31:06 -0300

    objetos pessoa adicionada

---
+++ src/sistemacadastro/Pessoa.java
@@ -0,0 +1,33 @@
+
+
+package sistemacadastro;
+ 
+ 
+
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+    private String cpf;
+
+    // funções abaixo
+    
+    
+    
+    //contrutores 
+    public Pessoa(String nome, int idade, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+    public Pessoa() {
+    }
+    
+    
+    // getd e sets 
+    
+    
+ 
+}
+
