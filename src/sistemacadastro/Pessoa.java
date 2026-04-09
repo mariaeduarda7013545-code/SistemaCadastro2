ecd0887b370e4b43d885774fa168b91d03b43842
arthurrodrigues130210-oss <arthurrodrigues130210@gmail.com>
2026-04-09 19:57:36 -0300

    novas funções e gets e sets adicionados

--- src/sistemacadastro/Pessoa.java
+++ src/sistemacadastro/Pessoa.java
@@ -11,6 +11,11 @@ public class Pessoa {
     private String cpf;
 
     // funções abaixo
+    public void exibirDados(){
+        System.out.println("Adicione o seu" + nome +
+                "Coloque sua idade" + idade +
+                "Insire seu CPF" + cpf);
+    }
     
     
     
@@ -27,6 +32,28 @@ public class Pessoa {
     
     // getd e sets 
     
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public void setIdade(int idade){
+        if (idade >= 0){
+            this.idade = idade;
+        }else{
+            System.out.println("Idade invalida");
+        }
+    }
+    public String getCpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
     
  
 }
