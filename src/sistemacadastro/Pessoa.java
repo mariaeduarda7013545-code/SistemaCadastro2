6eada487f90541254a1cf6246b5cb6b617c68fb2
$Madu$ <mariaeduarda7013545@gmail.com>
2026-04-13 19:53:15 -0300

    Alteração Visual

--- src/sistemacadastro/Pessoa.java
+++ src/sistemacadastro/Pessoa.java
@@ -1,24 +1,18 @@
-
-
 package sistemacadastro;
- 
- 
 
 public class Pessoa {
-    
+
     private String nome;
     private int idade;
     private String cpf;
 
     // funções abaixo
-    public void exibirDados(){
-        System.out.println("Adicione o seu" + nome +
-                "Coloque sua idade" + idade +
-                "Insire seu CPF" + cpf);
+    public void exibirDados() {
+        System.out.println("Adicione o seu" + nome);
+        System.out.println("Coloque sua idade" + idade);        
+        System.out.println("Insire seu CPF" + cpf);        
     }
-    
-    
-    
+
     //contrutores 
     public Pessoa(String nome, int idade, String cpf) {
         this.nome = nome;
@@ -28,33 +22,34 @@ public class Pessoa {
 
     public Pessoa() {
     }
-    
-    
+
     // getd e sets 
-    
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
-    public void setNome(String nome){
+
+    public void setNome(String nome) {
         this.nome = nome;
     }
-    public int getIdade(){
+
+    public int getIdade() {
         return idade;
     }
-    public void setIdade(int idade){
-        if (idade >= 0){
+
+    public void setIdade(int idade) {
+        if (idade >= 0) {
             this.idade = idade;
-        }else{
+        } else {
             System.out.println("Idade invalida");
         }
     }
-    public String getCpf(){
+
+    public String getCpf() {
         return cpf;
     }
-    public void setCpf(String cpf){
+
+    public void setCpf(String cpf) {
         this.cpf = cpf;
     }
-    
- 
-}
 
+}
